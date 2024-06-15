c1e4bdeff94ba8d9c3e11f37104e6208a592e807
eunbinnie <dmsqlsdl54@naver.com>
2024-06-15 19:59:32 +0900

    fix: 드롭다운 컴포넌트 변경

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -10,7 +10,6 @@ type PropsType = {
 };
 
 const Dropdown = (props: PropsType) => {
-  const DropdownRef = useRef<HTMLDivElement | null>(null);
   const { list, setOption } = props;
   const [view, setView] = useState(false);
   const [selected, setSelected] = useState(list[0].text);
@@ -30,49 +29,41 @@ const Dropdown = (props: PropsType) => {
     setView(false);
   };
 
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (
-        DropdownRef.current &&
-        !DropdownRef.current.contains(event.target as Node)
-      ) {
-        setView(false);
-      }
-    };
-
-    document.addEventListener("click", handleClickOutside);
-    return () => {
-      document.removeEventListener("click", handleClickOutside);
-    };
-  }, [DropdownRef]);
-
   return (
-    <div className="relative" ref={DropdownRef}>
-      <button
-        onClick={handleDropDownClick}
-        className="w-[130px] px-5 py-2 cursor-pointer border border-gray-200 rounded-xl flex justify-between"
-      >
-        <span>{selected}</span>
-        <div className="w-6 h-6 flex justify-center items-center">
-          <Image src={toggleIcon} alt="더보기" />
-        </div>
-      </button>
-      {view && (
-        <ul className="absolute mt-1 w-[130px] border border-gray-200 rounded-xl bg-white z-[1] transition-transform">
-          {list?.map((i, index) => {
-            return (
-              <li
-                key={index}
-                onClick={handleOptionClick(i.key)}
-                className="cursor-pointer text-center leading-[1.5] py-[9px] border-b border-b-gray-200 last-of-type:border-b-0"
-              >
-                {i.text}
-              </li>
-            );
-          })}
-        </ul>
-      )}
-    </div>
+    <>
+      <div className="relative">
+        <button
+          onClick={handleDropDownClick}
+          className="w-[130px] px-5 py-2 cursor-pointer border border-gray-200 rounded-xl flex justify-between"
+        >
+          <span>{selected}</span>
+          <div className="w-6 h-6 flex justify-center items-center">
+            <Image src={toggleIcon} alt="더보기" />
+          </div>
+        </button>
+        {view && (
+          <>
+            <ul className="absolute mt-1 w-[130px] border border-gray-200 rounded-xl bg-white z-[9999]">
+              {list?.map((i, index) => {
+                return (
+                  <li
+                    key={index}
+                    onClick={handleOptionClick(i.key)}
+                    className="cursor-pointer text-center leading-[1.5] py-[9px] border-b border-b-gray-200 last-of-type:border-b-0"
+                  >
+                    {i.text}
+                  </li>
+                );
+              })}
+            </ul>
+            <div
+              onClick={handleDropDownClick}
+              className="fixed w-full h-full top-0 left-0 z-[10] opacity-25"
+            ></div>
+          </>
+        )}
+      </div>
+    </>
   );
 };
 
